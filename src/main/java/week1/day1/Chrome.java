f8e20dbdace03d5156b26bcf6ea3778abec545ce
Kavi <kaviip97@gmail.com>
2025-11-09 09:31:48 +0530

    Added Chrome.java

---
+++ src/main/java/week1/day1/Chrome.java
@@ -0,0 +1,16 @@
+package week1.day1;
+
+public class Chrome {
+	public static void main(String[] args) {
+		float version = 91.0f;
+		String developer = "Google";
+		Boolean isBeta = false;
+		int releaseYear = 2008;
+		char shortCutKey = 'C';
+		System.out.println(version);
+		System.out.println(developer);
+		System.out.println(isBeta);
+		System.out.println(releaseYear);
+		System.out.println(shortCutKey);
+	}
+}
